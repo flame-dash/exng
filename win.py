681320d099922ac350ffb9fa5de3908b6373811b
flame-dash <ppPrompt@hotmail.com>
2024-12-24 19:09:02 +0700

    Add files via upload

---
+++ win.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import *
+
+
+def exng():
+    window = Tk()
+    window.title("Windows Application")
+    window.geometry("800x800")
+    window.mainloop()
+def pack():
+    print("was pack")
+    input("948948338983949")
